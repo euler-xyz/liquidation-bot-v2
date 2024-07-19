1a5af60fc9002a14d47613c31c2685e622b0236a
Lee <lee@euler.xyz>
2024-07-19 15:19:32 +0200

    working on scenario script

--- contracts/DeployLiquidator.sol
+++ contracts/DeployLiquidator.sol
@@ -15,9 +15,13 @@ contract DeployLiquidator is Script {
 
         uint256 deployerPrivateKey = vm.deriveKey(vm.envString("MNEMONIC"), 0);
         
-        address swapperAddress = vm.envAddress("SWAPPER_ADDRESS");
-        address swapVerifierAddress = vm.envAddress("SWAP_VERIFIER_ADDRESS");
-        address evcAddress = vm.envAddress("EVC_ADDRESS");
+        // address swapperAddress = vm.envAddress("SWAPPER_ADDRESS");
+        // address swapVerifierAddress = vm.envAddress("SWAP_VERIFIER_ADDRESS");
+        // address evcAddress = vm.envAddress("EVC_ADDRESS");
+
+        address swapperAddress = 0xf4e55515952BdAb2aeB4010f777E802D61eB384f;
+        address swapVerifierAddress = 0xe519389F8c262d4301Fd2830196FB7D0021daf59;
+        address evcAddress = 0x72F853E9E202600c5017B5A060168603c3ed7368;
 
         deployer = vm.addr(deployerPrivateKey);
         vm.startBroadcast(deployerPrivateKey);
--- test/BasicLiquidationScenario.sol
+++ test/BasicLiquidationScenario.sol
@@ -13,24 +13,23 @@ import {MockPriceOracle} from "../contracts/MockPriceOracle.sol";
 import {GenericFactory} from "../contracts/GenericFactory/GenericFactory.sol";
 
 contract BasicScenario is Test, Script {
-    address constant TEST_VAULT_1_ADDRESS = 0xf6cb30F1b333B511be23f6Fc0b733ed26030d6f7;
-    address constant TEST_VAULT_1_UNDERLYING = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
-    address constant TEST_VAULT_2_ADDRESS = 0x10F9509d401dedb0605616B89cfE26FA614084B7;
-    address constant TEST_VAULT_2_UNDERLYING = 0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984;
-    address constant ORACLE_ADDRESS = 0x0C8542AB89c1C60D711B00F309f7EF63b5D9d6eb;
-    address constant FACTORY_ADDRESS = 0x6c383Ef7C9Bf496b5c847530eb9c49a3ED6E4C56;
+    address constant TEST_VAULT_1_ADDRESS = 0x21657b967dFae90c6a1ec51D7cfa659B95291F6f;
+    address constant TEST_VAULT_1_UNDERLYING = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    
+    address constant TEST_VAULT_2_ADDRESS = 0xDb17C6bBD90D09011FB8B8F83E98d906371D1930;
+    address constant TEST_VAULT_2_UNDERLYING = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
 
-    string constant MNEMONIC = "test test test test test test test test test test test junk";
+    address constant ORACLE_ADDRESS = 0xE57A305f34fD0B6A55A66e8ec9559e6573100cBe;
     
     address deployer;
+    address borrower;
+    address liquidator;
 
     IEVault vault1;
     IEVault vault2;
 
     IERC20 underlying1;
     IERC20 underlying2;
-
-    GenericFactory factory;
 
     MockPriceOracle oracle;
 
@@ -43,29 +42,85 @@ contract BasicScenario is Test, Script {
 
         oracle = MockPriceOracle(ORACLE_ADDRESS);
 
-        factory = GenericFactory(FACTORY_ADDRESS);
-
-        uint256 deployerPrivateKey = vm.deriveKey(MNEMONIC, 0);
+        uint256 deployerPrivateKey = vm.deriveKey(vm.envString("MNEMONIC"), 0);
+        
         deployer = vm.addr(deployerPrivateKey);
 
         vm.startBroadcast(deployerPrivateKey);
 
-        console.log("underlying1 balance: ", underlying1.balanceOf(deployer));
-        console.log("underlying2 balance: ", underlying2.balanceOf(deployer));
 
-        console.log("msg sender: ", msg.sender);
-        console.log("sender balance: ", underlying1.balanceOf(msg.sender));
+        uint256 liquidatorPrivateKey = vm.deriveKey(vm.envString("MNEMONIC"), 1);
+        liquidator = vm.addr(liquidatorPrivateKey);
 
-        underlying1.approve(address(vault1), type(uint).max);
-        underlying2.approve(address(vault2), type(uint).max);
+        uint256 borrowerPrivateKey = vm.deriveKey(vm.envString("MNEMONIC"), 2);
+        borrower = vm.addr(borrowerPrivateKey);
+
+        // console.log("deployer public key: ", deployer);
+        // console.log("liquidator: ", liquidator);
+        // console.log("borrower: ", borrower);
+        
+        console.log("Initial state:");
+        logState();
+
+        distributeTokens();
+
+        console.log("After distributing tokens:");
+        logState();
+
+        console.log("Vault1 code length", TEST_VAULT_1_ADDRESS.code.length);
+        console.log("Vault2 code length", TEST_VAULT_2_ADDRESS.code.length);
+
+        console.log(vault1.asset());
+        // depositInVaults();
+        
+        // console.log("After depositing in vaults:");
+        // logState();
+        
+        // underlying1.transfer(borrower, 1e10);
+        // underlying2.approve(address(vault2), type(uint).max);
 
         // IEVault vault = IEVault(
         //     factory.createProxy(address(0), true, abi.encodePacked(address(underlying1), address(oracle), address(1)))
         // );
 
         // console.log(vault1.asset());
-        console.log(underlying1.name());
+        // console.log(underlying1.name());
 
         // vault1.setLTV(address(vault2), 0.9e4, 0.9e4, 0);
+    }
+
+    function depositInVaults() internal {
+        underlying1.approve(address(vault1), type(uint).max);
+        underlying2.approve(address(vault2), type(uint).max);
+
+        vault1.deposit(1e10, msg.sender);
+        vault2.deposit(1e10, msg.sender);
+    }
+
+    function distributeTokens() internal {
+        underlying1.transfer(borrower, 1e10);
+        underlying2.transfer(borrower, 1e10);
+    }
+
+    function logState() internal {
+        console.log(" ");
+        console.log("Deployer:");
+        // console.log("Vault 1 balance: ", vault1.balanceOf(deployer));
+        console.log("Underlying 1 balance: ", underlying1.balanceOf(deployer));
+        // console.log("Vault 2 balance: ", vault2.balanceOf(deployer));
+        console.log("Underlying 2 balance: ", underlying2.balanceOf(deployer));
+        console.log("--------------------");
+        console.log("Borrower:");
+        // console.log("Vault 1 balance: ", vault1.balanceOf(borrower));
+        console.log("Underlying 1 balance: ", underlying1.balanceOf(borrower));
+        // console.log("Vault 2 balance: ", vault2.balanceOf(borrower));
+        console.log("Underlying 2 balance: ", underlying2.balanceOf(borrower));
+        console.log("--------------------");
+        console.log("Liquidator:");
+        // console.log("Vault 1 balance: ", vault1.balanceOf(liquidator));
+        console.log("Underlying 1 balance: ", underlying1.balanceOf(liquidator));
+        // console.log("Vault 2 balance: ", vault2.balanceOf(liquidator));
+        console.log("Underlying 2 balance: ", underlying2.balanceOf(liquidator));
+        console.log("----------------------------------------");
     }
 }
