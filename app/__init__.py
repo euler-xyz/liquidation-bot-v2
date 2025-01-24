6baac65e07412e0735e5b80cd642e5f2e19bfb89
Lee <lee@euler.xyz>
2025-01-24 12:18:11 -0800

    adding chain

--- app/__init__.py
+++ app/__init__.py
@@ -15,7 +15,7 @@ def create_app():
     def health_check():
         return jsonify({"status": "healthy"}), 200
     
-    chain_ids = [1923]
+    chain_ids = [1923, 8453]
 
     monitor_thread = threading.Thread(target=start_monitor, args=(chain_ids,))
     monitor_thread.start()
