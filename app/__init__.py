b10b25889ae4b139388c336c572ba8a560d4a7e8
dangow3 <daniel.d@protokol.com>
2025-04-03 14:54:06 +0300

    Added BOB.

--- app/__init__.py
+++ app/__init__.py
@@ -17,17 +17,18 @@ def create_app():
     
     chain_ids = [
         1, # Ethereum mainnet
-        10, # Optimism
-        56, # Binance Smart Chain
-        100, # Gnosis Chain
-        130, # Unichain
-        137, # Polygon
-        480, # World Chain
-        8453, # Base
-        42161, # Arbitrum One
-        43114, # Avalanche
-        57073, # Ink
         80094, # Berachain
+        60808, # BOB
+        # 10, # Optimism
+        # 56, # Binance Smart Chain
+        # 100, # Gnosis Chain
+        # 130, # Unichain
+        # 137, # Polygon
+        # 480, # World Chain
+        # 8453, # Base
+        # 42161, # Arbitrum One
+        # 43114, # Avalanche
+        # 57073, # Ink
         ]
 
     monitor_thread = threading.Thread(target=start_monitor, args=(chain_ids,))
