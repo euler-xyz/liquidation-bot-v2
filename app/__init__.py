c628532c063af4d2d0341b3a9ac946c16400b19e
dangow3 <daniel.d@protokol.com>
2025-04-22 12:15:13 +0300

    Setup done.

--- app/__init__.py
+++ app/__init__.py
@@ -15,7 +15,11 @@ def create_app():
     def health_check():
         return jsonify({"status": "healthy"}), 200
     
-    chain_ids = [1]
+    chain_ids = [
+         1, # Ethereum mainnet
+        80094, # Berachain
+        60808, # BOB
+    ]
     # chain_ids = [80094]
     monitor_thread = threading.Thread(target=start_monitor, args=(chain_ids,))
     monitor_thread.start()
