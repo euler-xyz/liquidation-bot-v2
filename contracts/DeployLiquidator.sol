30f309bfa33ca9ffc2920c4bf5d8f59a94a1ccc2
Lee <lee@euler.xyz>
2025-05-03 08:41:27 -0700

    updated liquidator to disable collateral

--- contracts/DeployLiquidator.sol
+++ contracts/DeployLiquidator.sol
@@ -13,9 +13,9 @@ contract DeployLiquidator is Script {
     function run() public {
         uint256 deployerPrivateKey = vm.envUint("LIQUIDATOR_PRIVATE_KEY");
 
-        address swapperAddress = 0x05Eb1A647265D974a1B0A57206048312604Ac6C3;
-        address swapVerifierAddress = 0x392C1570b3Bf29B113944b759cAa9a9282DA12Fe;
-        address evcAddress = 0x08739CBede6E28E387685ba20e6409bD16969Cde;
+        address swapperAddress = 0x2Bba09866b6F1025258542478C39720A09B728bF;
+        address swapVerifierAddress = 0xae26485ACDDeFd486Fe9ad7C2b34169d360737c7;
+        address evcAddress = 0x0C9a3dd6b8F28529d72d7f9cE918D493519EE383;
         address pyth = 0x4305FB66699C3B2702D4d05CF36551390A4c69C6;
 
         address deployer = vm.addr(deployerPrivateKey);
