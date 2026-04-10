0847e88893c1e28f23dab9978343d329ee082c37
xBA5ED <83727748+xBA5ED@users.noreply.github.com>
2026-04-10 14:06:17 +0200

    feat: do not perform a swap when collateral and debt are the same token

--- app/__init__.py
+++ app/__init__.py
@@ -29,7 +29,10 @@ def create_app():
         except ValueError:
             chain_ids = None
             logging.info(f"Starting monitor with default chain id")
-
+    
+    # Temp override for playground.
+    chain_ids = [8453]
+    
     monitor_thread = threading.Thread(target=start_monitor, args=(chain_ids,))
     monitor_thread.start()
 
