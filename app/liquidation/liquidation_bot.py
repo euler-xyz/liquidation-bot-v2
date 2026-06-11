c14545cbfebac54d7fc028e6869be30b48d7f562
0xBA5ED.eth <83727748+xBA5ED@users.noreply.github.com>
2026-06-11 13:14:56 +0200

    disable slack notification for unhealthy accounts (#52)

--- app/liquidation/liquidation_bot.py
+++ app/liquidation/liquidation_bot.py
@@ -525,10 +525,10 @@ class AccountMonitor:
                                         health_score,
                                     )
                                 else:
-                                    logger.info("Posting unhealthy account notification to slack for %s %s %s", self.chain_id, account.controller.vault_symbol, health_score)
-                                    post_unhealthy_account_on_slack(address, account.controller.address,
-                                                                    health_score,
-                                                                    account.value_borrowed, self.config)
+                                    logger.info("Unhealthy account %s %s %s", self.chain_id, account.controller.vault_symbol, health_score)
+                                    # post_unhealthy_account_on_slack(address, account.controller.address,
+                                    #                                 health_score,
+                                    #                                 account.value_borrowed, self.config)
                                     logger.info("Valut borrowed: %s", account.value_borrowed)
                                     if account.value_borrowed < self.config.SMALL_POSITION_THRESHOLD:
                                         self.recently_posted_low_value[account.address] = time.time()
